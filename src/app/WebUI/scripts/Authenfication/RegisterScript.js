f41f9474a4e86075c616dafed21b850e666f0b0e
MonkeyFonkey1 <andrevirlan@yahoo.com>
2023-11-17 01:42:15 +0200

    Small fixes for Register, and login (work in progress)

--- src/app/WebUI/scripts/Authenfication/RegisterScript.js
+++ src/app/WebUI/scripts/Authenfication/RegisterScript.js
@@ -1,11 +1,12 @@
-document.getElementById('dateOfBirth').addEventListener('input', function(event) {
-    var inputValue = event.target.value;
-    if (inputValue.length === 8) {
-        var formattedDate = inputValue.replace(/(\d{2})(\d{2})(\d{4})/, '$1/$2/$3');
-        event.target.value = formattedDate;
-    }
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('dateOfBirth').addEventListener('input', function(event) {
+        var inputValue = event.target.value;
+        if (inputValue.length === 8) {
+            var formattedDate = inputValue.replace(/(\d{2})(\d{2})(\d{4})/, '$1/$2/$3');
+            event.target.value = formattedDate;
+        }
+    });
 });
-
 
 var passwordInput = document.getElementById("password");
 var confirmPasswordInput = document.getElementById("confirmPassword");
@@ -14,11 +15,20 @@ function validatePasswordMatch() {
     var password = passwordInput.value;
     var confirmPassword = confirmPasswordInput.value;
 
-    if (password !== confirmPassword) {
+  
+    
+    if (password !== confirmPassword){
         confirmPasswordInput.classList.add("password-mismatch");
         passwordInput.value = '';
         confirmPasswordInput.value = '';
     } else {
         confirmPasswordInput.classList.remove("password-mismatch");
+        console.log("Redirecting to login page");
+        redirectToLoginPage();
     }
+}
+
+function redirectToLoginPage() {
+    //console.log("Redirecting to login page");
+    window.location.href = "/src/app/WebUI/html/AuthentificationPanels/LoginPage.html";
 }
