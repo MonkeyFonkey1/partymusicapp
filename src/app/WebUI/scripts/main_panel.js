1ae418da1e838dadb1067432e5030df36310ba8f
MonkeyFonkey1 <andrevirlan@yahoo.com>
2023-11-16 23:15:54 +0200

    Small change in mainpage. Added the fetch for userID for main panel

--- src/app/WebUI/scripts/main_panel.js
+++ src/app/WebUI/scripts/main_panel.js
@@ -1,16 +1,19 @@
-// script.js
-
 document.addEventListener('DOMContentLoaded', function () {
-
-    // Fetch user data
-    fetch('http://localhost:9090/pma/api/v1/users/{userId}')
-        .then(response => response.json())
-        .then(user => {
-            document.getElementById('username-placeholder').innerText = user.username;
-            document.getElementById('follower').innerText = 'Followers: ' + user.noOfFollowers; // Update with the actual field names
-            document.getElementById('following').innerText = 'Following: ' + user.noOfFollowing;
-            document.getElementById('parties_hosted').innerText = 'Parties Hosted: ' + user.noOfHostedParties;
-            document.getElementById('parties_participated').innerText = 'Parties Participated: ' + user.noOfJoinedParties;
+    // Fetch current user ID
+    fetch('http://localhost:9090/pma/api/v1/users/userid')
+        .then(response => response.text())
+        .then(userId => {
+            // Fetch user data based on the retrieved user ID
+            fetch(`http://localhost:9090/pma/api/v1/users/userid/${userId}`)
+                .then(response => response.json())
+                .then(user => {
+                    document.getElementById('username-placeholder').innerText = user.username;
+                    document.getElementById('follower').innerText = 'Followers: ' + user.noOfFollowers;
+                    document.getElementById('following').innerText = 'Following: ' + user.noOfFollowing;
+                    document.getElementById('parties_hosted').innerText = 'Parties Hosted: ' + user.noOfHostedParties;
+                    document.getElementById('parties_participated').innerText = 'Parties Participated: ' + user.noOfJoinedParties;
+                })
+                .catch(error => console.error('Error fetching user data:', error));
         })
-        .catch(error => console.error('Error fetching user data:', error));
+        .catch(error => console.error('Error fetching current user ID:', error));
 });
