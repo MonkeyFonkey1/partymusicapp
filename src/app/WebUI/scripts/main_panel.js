2b670c77f3301ea5016b8cc0919c6dab148881de
MonkeyFonkey1 <andrevirlan@yahoo.com>
2023-11-19 14:07:52 +0200

    Fixed the fetch for user data + added fetch for party

--- src/app/WebUI/scripts/main_panel.js
+++ src/app/WebUI/scripts/main_panel.js
@@ -1,20 +1,56 @@
 
-document.addEventListener('DOMContentLoaded', function () {
-    // Fetch current user ID
-    fetch('http://localhost:9090/pma/api/v1/users/userid')
-        .then(response => response.text())
-        .then(userId => {
-            // Fetch user data based on the retrieved user ID
-            fetch(`http://localhost:9090/pma/api/v1/users/userid/${userId}`)
-                .then(response => response.json())
-                .then(user => {
-                    document.getElementById('username-placeholder').innerText = user.username;
-                    document.getElementById('follower').innerText = 'Followers: ' + user.noOfFollowers;
-                    document.getElementById('following').innerText = 'Following: ' + user.noOfFollowing;
-                    document.getElementById('parties_hosted').innerText = 'Parties Hosted: ' + user.noOfHostedParties;
-                    document.getElementById('parties_participated').innerText = 'Parties Participated: ' + user.noOfJoinedParties;
-                })
-                .catch(error => console.error('Error fetching user data:', error));
-        })
-        .catch(error => console.error('Error fetching current user ID:', error));
-});
+const bearerToken = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJkYXZpZCIsImlhdCI6MTcwMDMzMTQzOSwiZXhwIjoxNzAwMzMyODc5fQ.zIQxlnClOANZi8chEGLimKzm-ofJ2tB8OgkS1KniKXw'; // Replace with your actual bearer token
+
+// Fetch user data
+fetch('http://localhost:9090/pma/api/v1/users/all', {
+    method: 'GET',
+    headers: {
+        'Authorization': `Bearer ${bearerToken}`,
+        'Content-Type': 'application/json'
+    }
+})
+    .then(response => response.json())
+    .then(users => {
+        // Update elements for each user in the array
+        users.forEach(user => {
+            document.getElementById("username-homepage").innerText = `Username: @${user.username} `;
+            document.getElementById("username-placeholder").innerText = user.username;
+            document.getElementById("follower").innerText = `Followers: ${user.noOfFollowers}`;
+            document.getElementById("following").innerText = `Following: ${user.noOfFollowing}`;
+            document.getElementById("parties_hosted").innerText = `Parties Hosted: ${user.noOfHostedParties}`;
+            document.getElementById("parties_participated").innerText = `Parties Participated: ${user.noOfJoinedParties}`;
+        });
+    })
+        .catch(error => console.error('Error fetching user data:', error));
+
+
+//get all parties
+fetch(`http://localhost:9090/pma/api/v1/parties/all`, {
+        method: 'GET',
+        headers:{
+            'Authorization': `Bearer ${bearerToken}`,
+            'Content-Type': 'application/json'
+        } 
+    })
+    .then(response => response.json())
+    .then(
+        parties => {
+        const partyListElement=document.getElementById('all_party_list');
+
+        if(Array.isArray(parties) && parties.length>0){
+
+            parties.forEach(party => {
+                const listItem=document.createElement('li');
+                listItem.textContent= `${party.title} - owner: ${party.ownerUsername}`;
+
+                partyListElement.appendChild(listItem);
+            })
+        }
+        else {
+            const listItem=document.createElement('li');
+                listItem.textContent= `${party.title} - owner: ${party.ownerUsername}`;
+
+                partyListElement.appendChild(listItem);
+        }
+    })
+    .catch(error => console.error(`Error fetching all parties`, error));
